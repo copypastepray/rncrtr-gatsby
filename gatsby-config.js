d363b8ac42ce5332abf7f5bd527d855f9e7782c5
rncrtr <ryan@copypastepray.com>
2021-06-29 12:32:14 -0600

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,23 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `The Localhost Blog`,
+    description: `This is my coding blog where I write about my coding journey.`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+          name: `posts`,
+          path: `${__dirname}/src/posts`,
+      },
+    },
+  ],
 }
